38827e0277699e7cb9981ef2012fec5efbc128b2
manhng83vn <manhng83vn@hotmail.com>
2020-09-26 09:42:24 +0700

    second commit

---
+++ OOP.Scheduler/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using log4net;
+using Quartz;
+using Quartz.Impl;
+
+namespace OOP.Scheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Scheduler scheduler = new Scheduler();
+            if (!scheduler.CheckScheduleStart())
+            {
+                if (int.TryParse(System.Configuration.ConfigurationManager.AppSettings["EveryDayAtHour"], out int hour)
+                    && int.TryParse(System.Configuration.ConfigurationManager.AppSettings["EveryDayAtMinute"], out int minute))
+                {
+                    scheduler.Start(hour, minute);
+                    Logger.Log.Info("Bắt đầu.");
+                    Console.WriteLine("Bắt đầu.");
+                }
+            }
+            else
+            {
+                scheduler.Stop();
+                Logger.Log.Info("Kết thúc.");
+                Console.WriteLine("Kết thúc.");
+            }
+        }
+    }
+
+    class Job : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                Logger.Log.Info("Thực hiện Job và kết thúc");
+                Console.WriteLine("Thực hiện Job và kết thúc");
+                System.Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                Logger.Log.Error(ex.ToString());
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+    class Scheduler
+    {
+        IScheduler scheduler;
+        IJobDetail job;
+        ITrigger trigger;
+
+        public void Start(int hour, int minute)
+        {
+            scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+            job = JobBuilder.Create<Job>().Build();
+
+            trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                  (s =>
+                     s.WithIntervalInHours(24)
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(hour, minute))
+                  )
+                .Build();
+            scheduler.ScheduleJob(job, trigger);
+        }
+
+        public bool CheckScheduleStart()
+        {
+            scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            return scheduler.IsStarted;
+        }
+
+        public void Stop()
+        {
+            if (scheduler.IsStarted)
+            {
+                scheduler.Shutdown();
+            }
+        }
+    }
+
+    public class Logger
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Logger));
+
+        public static ILog Log
+        {
+            get { return Logger.log; }
+        }
+    }
+}
---
+++ OOP.Scheduler/ReadMe.txt
@@ -0,0 +1,8 @@
+﻿Quartz: Scheduler in .Net
+Install-Package Quartz -Version 1.4.1
+
+ObjectsComparer: Using ObjectsComparer to compare objects in .Net
+Install-Package ObjectsComparer -Version 2.6.1
+
+log4net: Logging for .Net
+Install-Package log4net -Version 2.0.11
